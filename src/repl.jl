03736002b5c21533e63cbe155940cfb28ab8eb2e
Iaggelis <john_agelis@hotmail.com>
2020-07-29 11:54:20 +0300

    Improve repl functionality

--- src/repl.jl
+++ src/repl.jl
@@ -9,18 +9,38 @@ export local_repl
 
 function local_repl(tcp_sock::TCPSocket)
     server = listen(6969)
+    chn = ""
+    m = ""
     while true
         sock = accept(server)
         @async begin
             try
-                write(sock,"Connected\r\n")
+                write(sock,"Connected to bot repl\r\n")
                 while true
+                    write(sock, "#> ")
                     str = readline(sock)
-                    write(sock,"$str\r\n")
-                    if str[1:5] == "!JOIN"
-                        chn = str[7:end]
-                        write(sock,"$chn\r\n")
-                        irc_join(tcp_sock, chn)
+                    try
+                        m = match(r"(.*) (.*)", str)
+                        if m.captures[1] == "join"
+                            chn = m.captures[2]
+                            err = irc_join(tcp_sock, String(chn))
+                            println(stdout, "irc_join: ", err)
+                        elseif m.captures[1] == "part"
+                            chn = m.captures[2]
+                            irc_send(tcp_sock, "PART #$chn")
+                        elseif occursin("say", m.captures[1])
+                            msg = split(str, "say", limit=2)[2]
+                            irc_send(tcp_sock, String(chn), String(msg))
+                        end
+                    catch ex
+                        println(sock, "Exception on: ", str)
+                    end
+                    if str == "ls"
+                        if chn != ""
+                            println(sock, "Channel: ", chn)
+                        else
+                            println(sock, "No channel joined")
+                        end
                     end
                 end
             catch y
