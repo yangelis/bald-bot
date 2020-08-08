5f3a1b983e9f2a64f5ba3907e68c0d8f967ec26d
Iaggelis <john_agelis@hotmail.com>
2020-08-08 21:11:19 +0300

    No need to include tcp module

--- src/julia_bot.jl
+++ src/julia_bot.jl
@@ -1,21 +1,17 @@
 module julia_bot
 
-include("tcp.jl")
 include("irc.jl")
 include("repl.jl")
 
 using .irc
-using .tcp
 using .repl
 
 using Sockets, Base.Threads
-
 
 export bot_run
 
 const hostname = "irc.chat.twitch.tv"
 const nick = "baldiobot"
-const channel = "john_pft"
 const port = 6667
 
 function bot_run()
@@ -28,30 +24,6 @@ function bot_run()
     Threads.@spawn local_repl(tcp_sock)
     println(stdout, "Initialise:")
     err, msg = irc_readlines(tcp_sock)
-    # listening(tcp_sock)
-
 end
-
-function listening(tcp_sock)
-    println(stdout, "Start listening...")
-
-    while true
-        msg = ""
-        err, msg = irc_readlines(tcp_sock)
-        if err == Status(6)
-            break
-        end
-        if msg != nothing
-            println(stdout, msg)
-        end
-
-        # irc_send(tcp_sock, "PONG")
-        sleep(1)
-    end # while
-
-    st = tcp_close_sock(tcp_sock)
-    print(stdout, "Closing with status: ", st)
-end
-
 
 end # module
