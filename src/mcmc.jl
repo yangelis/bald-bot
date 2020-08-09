d4b3149736dabada29da7c09ef291117196d91ac
Iaggelis <john_agelis@hotmail.com>
2020-08-09 17:46:13 +0300

    Use a table struct instead of dictionary

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -6,41 +6,49 @@ module mcmc
 using Printf
 using Random
 
+
+mutable struct Table
+    prefixVec::Vector{Tuple{String, String}}
+    suffixVec::Vector{String}
+    Table() = new(Vector{Tuple{String,String}}(), Vector{String}())
+end
+
 function Table(s::String)
     splits = split(s)
-    buffer = Dict{Vector{String}, String}()
+    buffer = Table()
     for i in eachindex(splits[1:end-2])
-        prefixes, suffix = splits[i:i+1], splits[i+2]
-        if !haskey(buffer, prefixes)
-            buffer[prefixes] = suffix
+        prefixes, suffix = Tuple(splits[i:i+1]), splits[i+2]
+        if !(prefixes in buffer.prefixVec)
+            push!(buffer.prefixVec, prefixes)
+            push!(buffer.suffixVec, suffix)
         end
     end
     return buffer
 end
 
 function lookup(prefix, table)
-    if prefix in keys(table)
-        return table[prefix]
+   index = findfirst(x-> prefix == x, table.prefixVec)
+    if index != nothing
+        return table.suffixVec[index]
     end
     return ""
 end
 
-function generate(table)
+function generate(table::Table)
     buffer = ""
-    n::Int64 = 15
-    items = shuffle(collect(table))
-    currentPrefixes, currentSuffixes = items[1][1], items[1][2]
+    n::Int64 = rand(20:100)
+    items = shuffle(table.prefixVec), shuffle(table.suffixVec)
+    currentPrefixes, currentSuffixes = items[1][1], items[2][1]
     buffer = @sprintf("%s %s ", currentPrefixes[1], currentPrefixes[2])
 
     for i in 1:n
-        if currentSuffixes == "" || currentPrefixes == ["",""]
-            randomvec = rand(items)
-            currentPrefixes = randomvec[1]
-            currentSuffixes = randomvec[2]
+        if isempty(currentSuffixes)
+            currentPrefixes = rand(items[1])
+            currentSuffixes = rand(items[2])
         else
-            word = rand(items)[2]
+            word = rand(items[2])
             buffer *= @sprintf("%s ", word)
-            currentPrefixes = [currentPrefixes[2], word]
+            currentPrefixes = (currentPrefixes[2], word)
             currentSuffixes = lookup(currentPrefixes, table)
         end
     end
@@ -50,8 +58,8 @@ end
 
 function generate_from_string(s::String)
     temp = Table(s)
-    return generate(temp)
+    gen_text = generate(temp)
+    return gen_text
 end
-
 
 end # module
